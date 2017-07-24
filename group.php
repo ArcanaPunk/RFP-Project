f187387d977beaa2ed96a4a298a66002c1a83ac8
Unknown <hale.alexander.w@gmail.com>
2017-07-23 21:11:47 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/Mason'""
    This reverts commit 4e0bc3914dc537fe632347cde8dae5dc14f7d811.

--- group.php
+++ group.php
@@ -150,62 +150,7 @@
                 <!-- Form box for State -->
                 <tr height="40">
                   <td width="30%"> <label>State:</label> </td>
-                  <td>
-                    <select name="state" id="state" class="form-control">
-                      <option value="Default">Choose a State.</option>
-                      <option value="AL">Alabama</option>
-                      <option value="AK">Alaska</option>
-                      <option value="AZ">Arizona</option>
-                      <option value="AR">Arkansas</option>
-                      <option value="CA">California</option>
-                      <option value="CO">Colorado</option>
-                      <option value="CT">Connecticut</option>
-                      <option value="DE">Delaware</option>
-                      <option value="DC">District Of Columbia</option>
-                      <option value="FL">Florida</option>
-                      <option value="GA">Georgia</option>
-                      <option value="HI">Hawaii</option>
-                      <option value="ID">Idaho</option>
-                      <option value="IL">Illinois</option>
-                      <option value="IN">Indiana</option>
-                      <option value="IA">Iowa</option>
-                      <option value="KS">Kansas</option>
-                      <option value="KY">Kentucky</option>
-                      <option value="LA">Louisiana</option>
-                      <option value="ME">Maine</option>
-                      <option value="MD">Maryland</option>
-                      <option value="MA">Massachusetts</option>
-                      <option value="MI">Michigan</option>
-                      <option value="MN">Minnesota</option>
-                      <option value="MS">Mississippi</option>
-                      <option value="MO">Missouri</option>
-                      <option value="MT">Montana</option>
-                      <option value="NE">Nebraska</option>
-                      <option value="NV">Nevada</option>
-                      <option value="NH">New Hampshire</option>
-                      <option value="NJ">New Jersey</option>
-                      <option value="NM">New Mexico</option>
-                      <option value="NY">New York</option>
-                      <option value="NC">North Carolina</option>
-                      <option value="ND">North Dakota</option>
-                      <option value="OH">Ohio</option>
-                      <option value="OK">Oklahoma</option>
-                      <option value="OR">Oregon</option>
-                      <option value="PA">Pennsylvania</option>
-                      <option value="RI">Rhode Island</option>
-                      <option value="SC">South Carolina</option>
-                      <option value="SD">South Dakota</option>
-                      <option value="TN">Tennessee</option>
-                      <option value="TX">Texas</option>
-                      <option value="UT">Utah</option>
-                      <option value="VT">Vermont</option>
-                      <option value="VA">Virginia</option>
-                      <option value="WA">Washington</option>
-                      <option value="WV">West Virginia</option>
-                      <option value="WI">Wisconsin</option>
-                      <option value="WY">Wyoming</option>
-                    </select>
-                  </td>
+                  <td><input type="text" name="state" id="state" value="Full State Name" class="form-control"/></td>
                 </tr>
 
                 <!-- Form box for Game, is filled from the DB -->
@@ -213,7 +158,6 @@
                   <td width="30%"> <label>Game:</label> </td>
                   <td>
                     <select name="GameID" id="game" class="form-control">
-                      <option value="Default">Choose a Game</option>
                       <?php echo $options;?>
                     </select>
                   </td>
